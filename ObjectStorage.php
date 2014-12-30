1111ed772ca1be08988efb219d1fc27f5c1e7022
jv3 <jverity@jbrestaurants.com>
2014-12-30 13:29:25 -0500

    Created authenticate, copy and list container functions.

--- ObjectStorage.php
+++ ObjectStorage.php
@@ -1,43 +1,16 @@
 <?php
-/*
-Use cUrl to get API token and a URL that contains to full path to object storage account. 
-cUrl options set: url to connect to, custom http headers to send (username and password), set cUrl to accept any SSL server (https probz), set cUrl to output http header, set cUrl to output to string instead of just stdout;
-*/
-$curl = curl_init();
 
-curl_setopt($curl, CURLOPT_URL, "SET ME");
-curl_setopt($curl, CURLOPT_HTTPHEADER, array("X-Auth-User: SET ME", "X-Auth-Key: SET ME"));
-curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($curl, CURLOPT_HEADER, 1);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+require_once "functions.php";
 
-$curl_result = curl_exec($curl);
+//Authenticate function returns array containing $x_auth_token and $X_storage_url.
+//These 2 key/value pairs are extracted into variables for use in further object storage file operations.
 
-curl_close($curl);
+$auth_data = authenticate("SLOS292387-2:SL292387", "9f7586a9205d55cbf3d7a808f4693b8d637ef3a94984178869567671e391c9ec");
 
-/*
-Get the API token and url from the returned http header and store them in vars to use in future operations
-*/
+extract($auth_data);
 
-$x_auth_token = substr($curl_result, 67, 53);
-$x_storage_url = substr($curl_result, 180, 101);
+download("Backups", "test1", "file2.txt");
 
-/*
-Download a file from object storage
-*/
-
-$curl = curl_init();
-
-curl_setopt($curl, CURLOPT_URL, "https://dal05.objectstorage.softlayer.net/auth/v1.0");
-curl_setopt($curl, CURLOPT_HTTPHEADER, array("x_auth_token", "x_storage_url/test1/file1.txt"));
-curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($curl, CURLOPT_HEADER, 1);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-
-curl_setopt($curl, CURLOPT_FILE, fopen("file1.txt", "w"));
-
-curl_exec($curl);
-
-curl_close($curl);
+list_container("Backups");
 
 ?>
